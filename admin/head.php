2b95183bdf3b8bd17958d0cdd045f5beffab2b26
kiko923 <i@znnu.com>
2025-08-22 17:42:07 +0800

    修改静态文件CDN地址

--- admin/head.php
+++ admin/head.php
@@ -1,9 +1,7 @@
 <?php
 @header('Content-Type: text/html; charset=UTF-8');
 ?>
-<link rel="stylesheet" href="https://static.wicdn.com/layui/2.11.4/css/layui.css"/>
-<script src="https://static.wicdn.com/layui/2.11.4/layui.js" type="application/javascript"></script>
-<!--<script src="https://static.wicdn.com/jquery/3.6.0/jquery.min.js" type="application/javascript"></script>-->
-<!--<script src="https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/jquery/3.6.0/jquery.min.js" type="application/javascript"></script>-->
+<link rel="stylesheet" href="https://cdnjs.znnu.com/layui/2.11.4/css/layui.css"/>
+<script src="https://cdnjs.znnu.com/layui/2.11.4/layui.js" type="application/javascript"></script>
 <script src="https://cdnjs.znnu.com/jquery/3.7.1/jquery.min.js" integrity="sha384-1H217gwSVyLSIfaLxHbE7dRb3v4mYCKbpQvzx0cegeju1MVsGrX5xXxAvs/HgeFs" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<link href="https://img.wicdn.com/2023/12/06/avatar.jpeg" rel="icon">
+<link href="https://img.znnu.com/2023/12/06/avatar.jpeg" rel="icon">
